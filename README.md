37dbbe5f80003b5b2d857decadae4598a6f8a9d6
Dominic Prior <dominicprior42@gmail.com>
2025-06-16 16:02:58 +0100

    Tidy

--- README.md
+++ README.md
@@ -25,8 +25,3 @@ Based on https://www.youtufbe.com/watch?v=p4BHphMBlFA
 * `three/threebuild/three_core.js` from `r177 Three.js`.  Note the underscore instead of the dot.
 * Ditto for `three_module.js`.
 * `three/src/**/*.d.ts` from an `npm install @types/three`.
-
-* Copy `three.core.js` and `three.module.js` from `june-three-js` to `three/threebuild` as `three_core.js` and `three_module.js`.
-* Copy `three.core.d.ts` and `three.module.d.ts` from `C:\git\vite-demo-copy\node_modules\@types\three\build`, and change their (one-line) contents to lowercase and to underscores instead of dots.
-* Copy * from `vite-demo-copy/node_modules/@types/three/src` to `three/src`.
-* Add an importmap to `index.html`.
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
-import { square } from './foo.js'
 import { WebGLRenderer, PerspectiveCamera, Scene, Color } from '../three/threebuild/three_module.js';
-console.log(WebGLRenderer);
+console.log('dominic!');
 const renderer = new WebGLRenderer({ antialias: true });
 const scene = new Scene();
 scene.background = new Color('skyblue');
@@ -13,22 +9,9 @@ renderer.render(scene, camera);
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
+    hey!
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
   </div>
 `
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
-
-console.log(square(5))
