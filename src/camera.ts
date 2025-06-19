61b64fb0a6f5fd4f55e946b505e5f7752ba912c2
Dominic Prior <dominicprior42@gmail.com>
2025-06-19 09:09:28 +0100

    Move the near clipping plane

--- src/camera.ts
+++ src/camera.ts
@@ -6,7 +6,7 @@ function createCamera() {
     const camera = new PerspectiveCamera(
         35, // fov = Field Of View
         1, // aspect ratio (dummy value)
-        0.1, // near clipping plane
+        1, // near clipping plane
         10000, // far clipping plane
     );
 
