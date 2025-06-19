fe17a83b3e87d52f7ed67932d9c0c64ff032fb4f
Dominic Prior <dominicprior42@gmail.com>
2025-06-19 21:14:24 +0100

    Add main2.ts

---
+++ src/main2.ts
@@ -0,0 +1,27 @@
+console.log('dominic - main2.ts');
+import {
+    Scene, Color, PerspectiveCamera, WebGLRenderer,
+    PlaneGeometry, MeshBasicMaterial, Mesh, TextureLoader,
+} from '../three/threebuild/three_module.js';
+const container = document.querySelector('#scene-container');
+const scene = new Scene();
+scene.background = new Color('skyblue');
+const renderer = new WebGLRenderer({ antialias: true });
+container!.append(renderer.domElement);
+const w = window.innerWidth;
+const h = window.innerHeight;
+renderer.setSize(w, h);
+const camera = new PerspectiveCamera(45, w / h, 1, 1000);
+camera.position.set(0, 0, 5);
+camera.lookAt(0, 0, 0);
+const textureLoader = new TextureLoader();
+const texture = await textureLoader.loadAsync('/assets/uv-test-col.png');
+const groundGeometry = new PlaneGeometry(2, 2);
+const groundMaterial = new MeshBasicMaterial({ map: texture, });
+const ground = new Mesh(groundGeometry, groundMaterial);
+scene.add(ground);
+// const animate = () => {
+//     requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+// };
+// animate();
