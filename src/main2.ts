16f33d68ff7d92296abaa778156b9900ac10299f
Dominic Prior <dominicprior42@gmail.com>
2025-06-27 21:33:09 +0100

    Add a loadingManager

--- src/main2.ts
+++ src/main2.ts
@@ -1,6 +1,6 @@
 console.log('dominic - main2.ts');
 import {
-    Scene, Color, PerspectiveCamera, WebGLRenderer,
+    Scene, Color, PerspectiveCamera, WebGLRenderer, LoadingManager,
     PlaneGeometry, MeshBasicMaterial, Mesh, TextureLoader, SRGBColorSpace,
 } from '../three/threebuild/three_module.js';
 const container = document.querySelector('#scene-container');
@@ -14,8 +14,22 @@ renderer.setSize(w, h);
 const camera = new PerspectiveCamera(45, w / h, 1, 1000);
 camera.position.set(0, 0, 5);
 camera.lookAt(0, 0, 0);
-const textureLoader = new TextureLoader();
-const texture = await textureLoader.loadAsync('/assets/uv-test-col.png');
+const loadingManager = new LoadingManager();
+loadingManager.onStart = () => {
+    console.log('Loading started');
+};
+loadingManager.onLoad = () => {
+    console.log('Loading complete');
+};
+loadingManager.onProgress = (url, itemsLoaded, itemsTotal) => {
+    console.log(`Loading file: ${url}, items loaded: ${itemsLoaded}, total items: ${itemsTotal}`);
+};
+loadingManager.onError = (url) => {
+    console.error(`Error loading file: ${url}`);
+};
+const textureLoader = new TextureLoader(loadingManager);
+const texture  = textureLoader.load('/assets/uv-test-col.png');
+                 textureLoader.load('/assets/uv-test-bw.png');
 texture.colorSpace = SRGBColorSpace;
 const groundGeometry = new PlaneGeometry(2, 2);
 const groundMaterial = new MeshBasicMaterial({ map: texture, });
@@ -23,10 +37,10 @@ const ground = new Mesh(groundGeometry, groundMaterial);
 scene.add(ground);
 setTimeout(() => {
     renderer.render(scene, camera);
-}, 1);
+}, 500);
 // const animate = () => {
 //     requestAnimationFrame(animate);
 //     renderer.render(scene, camera);
 // };
 // animate();
-console.log('dominic - main2.ts done');
+// console.log('dominic - main2.ts done', texture2);
