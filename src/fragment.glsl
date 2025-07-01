5c9d7a7b21a110634612a2785c5361142b1650e2
Dominic Prior <dominicprior42@gmail.com>
2025-07-01 06:14:52 +0100

    Add random glsl function

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -2,6 +2,10 @@ uniform float uBlue;
 uniform vec3 uColor;
 uniform sampler2D uTexture;
 varying vec2 vUv;
+
+float random(vec2 st) {
+    return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453);
+}
 void main() {
     // vec2 a = vec2(3.0);
     // vec3 b = vec3(a.yx, 0.5);
