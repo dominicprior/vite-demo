7720e129de4eda0c8789873e8a11702b3d82d9ef
Dominic Prior <dominicprior42@gmail.com>
2025-07-04 09:50:20 +0100

    WIP

---
+++ src/game/camera.ts
@@ -0,0 +1,49 @@
+import {
+    PerspectiveCamera,
+    Scene, 
+} from '../../three/threebuild/three_module.js';
+import { OrbitControls } from '../../three/threebuild/OrbitControls.js';
+
+import Game from './game.js';
+import Sizes from "./utils/sizes.js";
+
+export default class Camera {
+    game: Game;
+    sizes: Sizes;
+    scene: Scene;
+    canvas: HTMLCanvasElement | null;
+    // @ts-ignore: no initializer
+    instance: PerspectiveCamera;
+    // @ts-ignore: no initializer
+    controls: OrbitControls;
+    constructor(game: Game) {
+        this.game = game;
+        this.sizes = game.sizes;
+        this.scene = game.scene;
+        this.canvas = game.canvas;
+        this.setInstance();
+        this.setOrbitControls();
+    }
+
+    setInstance() {
+        this.instance = new PerspectiveCamera(
+                75, this.sizes.width / this.sizes.height, 0.1, 1000);
+        this.instance.position.set(6, 4, 8);
+        this.scene.add(this.instance);
+    }
+
+    setOrbitControls() {
+        this.controls = new OrbitControls(this.instance, this.canvas!);  // note the non-null assertion operator '!'
+        this.controls.enableDamping = true;
+        this.controls.dampingFactor = 0.05;
+        this.controls.enableZoom = true;
+        this.controls.minDistance = 2;
+        this.controls.maxDistance = 20;
+        this.controls.maxPolarAngle = Math.PI / 2; // Limit vertical rotation
+    }
+
+    resize() {
+        this.instance.aspect = this.sizes.width / this.sizes.height;
+        this.instance.updateProjectionMatrix();
+    }
+}
--- src/game/game.ts
+++ src/game/game.ts
@@ -1,10 +1,16 @@
+import {
+    Scene, 
+} from '../../three/threebuild/three_module.js';
 import Sizes from "./utils/sizes.js";
 import Time from "./utils/time.js";
+import Camera from './camera.js';
 
 export default class Game {
     canvas: HTMLCanvasElement | null;
     sizes: Sizes;
     time: Time;
+    scene: Scene;
+    camera: Camera;
     constructor(canvas: (HTMLCanvasElement|null)) {
         Object.defineProperty(window, 'a', { value: { game: this },
             writable: true, });
@@ -12,18 +18,20 @@ export default class Game {
         this.canvas = canvas;
         this.sizes = new Sizes();
         this.time = new Time();
+        this.scene = new Scene();
+        this.camera = new Camera(this);
         this.sizes.on('resize', this.resize.bind(this));  // Note the 'bind' to ensure 'this' refers to the Game instance instead of the Sizes instance.
         // this.sizes.on('resize', () => {  // This is an alternative way to bind 'this' using an arrow function.
         //     this.resize();
         // })
         this.time.on('tick', () => {
-            console.log('tick', this.time.delta);
             this.update();
         })
     }
 
     resize() {
         console.log('resize!!!', this);
+        this.camera.resize()
         // if (this.canvas) {
         //     this.canvas.width = this.sizes.width * this.sizes.pixelRatio;
         //     this.canvas.height = this.sizes.height * this.sizes.pixelRatio;
@@ -33,7 +41,6 @@ export default class Game {
     }
 
     update() {
-        console.log('update!!!', this);
         // Update game logic here
     }
 }
