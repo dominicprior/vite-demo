4860f946ad5c6f5a73e608da32e90fe7009ce91f
Dominic Prior <dominicprior42@gmail.com>
2025-07-07 08:41:02 +0100

    Tidy resources.ts

--- src/game/utils/resources.ts
+++ src/game/utils/resources.ts
@@ -27,25 +27,22 @@ export default class Resources extends EventEmitter {
     }
     startLoading() {
         for (const source of this.sources) {
-            if (source.type === 'texture') { 
+            if (source.type === 'texture') {
                 this.loaders.textureLoader.load(
                     source.path as string,
                     (file: any) => {
-                        console.log(`Loaded texture: ${source.name}`);
-                        // this.sourceLoaded(source.name, file);
+                        this.sourceLoaded(source.name, file);
                     }
                 );
             } else if (source.type === 'cubeTexture') {
-                // this.loaders.cubeTextureLoader.path = '/static';
                 this.loaders.cubeTextureLoader.load(
                     source.path as string[],
                     (file: any) => {
-                        // this.sourceLoaded(source.name, file);
                         this.sourceLoaded(source, file);
                     },
                     undefined,
                     (error: any) => {
-                        console.error(`Errorz loading cube texture: ${source.name}`, error);
+                        console.error(`Error loading cube texture: ${source.name}`, error);
                     }
                 );
             }
@@ -55,7 +52,6 @@ export default class Resources extends EventEmitter {
         this.items[source.name] = file;
         this.loaded++;
         if (this.loaded === this.toLoad) {
-            console.log('All resources loaded');
             this.trigger('ready');
         }
     }
