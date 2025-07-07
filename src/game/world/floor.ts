9f2ae840546d2dcef1a9709db9b23473643816e2
Dominic Prior <dominicprior42@gmail.com>
2025-07-07 17:35:48 +0100

    Add a floor setTextureMaterial

--- src/game/world/floor.ts
+++ src/game/world/floor.ts
@@ -1,12 +1,12 @@
 import {
-    Scene, Mesh, PlaneGeometry, MeshStandardMaterial,
+    Scene, Mesh, PlaneGeometry, MeshStandardMaterial, DataTexture, RepeatWrapping,
 } from '../../../three/threebuild/three_module.js';
 
 import Game from '../game.js'
 import Debug from '../utils/debug.js';
 
 export default class Floor {
-    numRows: number = 1;
+    numRows: number = 2;
     debug: Debug;
     scene: Scene;
     // @ts-ignore: no initializer
@@ -41,8 +41,46 @@ export default class Floor {
         });
     }
 
+
+// .vertexColors : Boolean
+// Defines whether vertex coloring is used. Default is false. 
+// The engine supports RGB and RGBA vertex colors depending
+// on whether a three (RGB) or four (RGBA) component color 
+// buffer attribute is used.
+
+    setTextureMaterial() {  // not used
+
+        const width = 4;
+        const height = 4;
+
+        const size = width * height;
+        const data = new Uint8Array( 4 * size );
+
+        // const r = 255;
+        const g = 150;
+        const b = 150;
+
+        for ( let i = 0; i < size; i ++ ) {
+            const stride = i * 4;
+            data[ stride ] = 255 * Math.random();
+            data[ stride + 1 ] = g;
+            data[ stride + 2 ] = b;
+            data[ stride + 3 ] = 255;
+        }
+
+        const texture = new DataTexture( data, width, height );
+        texture.wrapS = RepeatWrapping;
+        texture.wrapT = RepeatWrapping;
+        texture.needsUpdate = true;
+
+        this.material = new MeshStandardMaterial({
+            map: texture,
+        });
+    }
+
     setMesh() {
         this.mesh = new Mesh(this.geometry, this.material);
+        this.mesh.name = 'floor';
         this.mesh.rotation.x = - Math.PI * 0.5;
         // this.mesh.receiveShadow = true
         this.scene.add(this.mesh);
