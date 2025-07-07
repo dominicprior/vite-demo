ecdaee0fb5db198c7a56af118176395243de21fb
Dominic Prior <dominicprior42@gmail.com>
2025-07-07 13:14:45 +0100

    Add floor numRows gui controller

--- src/game/world/floor.ts
+++ src/game/world/floor.ts
@@ -3,8 +3,11 @@ import {
 } from '../../../three/threebuild/three_module.js';
 
 import Game from '../game.js'
+import Debug from '../utils/debug.js';
 
 export default class Floor {
+    numRows: number = 1;
+    debug: Debug;
     scene: Scene;
     // @ts-ignore: no initializer
     geometry: PlaneGeometry;
@@ -14,27 +17,36 @@ export default class Floor {
     mesh: Mesh;
 
     constructor(game: Game) {
-        this.scene = game.scene
+        this.debug = game.debug;
+        this.scene = game.scene;
 
-        this.setGeometry()
-        this.setMaterial()
-        this.setMesh()
+        this.setGeometry();
+        this.setMaterial();
+        this.setMesh();
     }
 
     setGeometry() {
-        this.geometry = new PlaneGeometry(10, 10)
+        this.geometry = new PlaneGeometry(10, 10, this.numRows, this.numRows);
+        this.debug.gui.add(this, 'numRows', 1, 10, 1).name('Num floor rows')
+            .onChange(() => {
+                this.scene.remove(this.mesh);
+                this.geometry.dispose();
+                this.geometry = new PlaneGeometry(10, 10, this.numRows, this.numRows);
+                this.mesh.geometry = this.geometry;
+                this.scene.add(this.mesh);
+            });
     }
 
     setMaterial() {
         this.material = new MeshStandardMaterial({
             color: 'yellow',
-        })
+        });
     }
 
     setMesh() {
-        this.mesh = new Mesh(this.geometry, this.material)
-        this.mesh.rotation.x = - Math.PI * 0.5
+        this.mesh = new Mesh(this.geometry, this.material);
+        this.mesh.rotation.x = - Math.PI * 0.5;
         // this.mesh.receiveShadow = true
-        this.scene.add(this.mesh)
+        this.scene.add(this.mesh);
     }
 }
