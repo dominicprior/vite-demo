54ab7a7f0a552fee29ca492c20afee8acebb9610
Dominic Prior <dominicprior42@gmail.com>
2025-07-11 15:30:46 +0100

    Add horizontalbox.ts

---
+++ src/game/collisions/horizontalbox.ts
@@ -0,0 +1,41 @@
+import {
+    Vector2,
+} from '../../../three/threebuild/three_module.js';
+import type {
+    Player, Collision,
+} from '../utils/types.js';
+import {
+    noCollision,
+} from '../utils/types.js';
+import VerticalFace from './verticalface.js';
+
+export default class HorizontalBox {
+    centre: Vector2;
+    width: number;
+    depth: number;
+    angle: number;
+    constructor(centre: Vector2, width: number,
+                depth: number, angle: number) {
+        this.centre = centre;
+        this.width = width;
+        this.depth = depth;
+        this.angle = angle;
+    }
+    firstCollision(player: Player): Collision {
+        let result = noCollision;
+        let corners: Array<Vector2> = [[1,1], [1,-1], [-1,-1], [-1,1]].map(
+                a => (new Vector2(a[0], a[1]))
+                        .multiply(new Vector2(this.width / 2, this.depth / 2))
+                        .rotateAround(new Vector2(), this.angle)
+                        .add(this.centre)
+                    );
+        for (let i=0; i < 4; i++) {
+            let face = new VerticalFace(corners[i], corners[(i+1) % 4]);
+            let collision = face.firstCollision(player);
+            if (collision.t < result.t) {
+                result = collision;
+            }
+        }
+        return result;
+    }
+}
