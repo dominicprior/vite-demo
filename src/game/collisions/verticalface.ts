81b144a79d6291788bdf631294474468e6f42be2
Dominic Prior <dominicprior42@gmail.com>
2025-07-13 12:08:33 +0100

    Add extra condition to verticalface.ts

--- src/game/collisions/verticalface.ts
+++ src/game/collisions/verticalface.ts
@@ -9,8 +9,6 @@ import {
 } from '../utils/types.js';
 import Player from '../player.js';
 
-
-
 export default class VerticalFace {
     a: Vector2;
     b: Vector2;
@@ -22,6 +20,8 @@ export default class VerticalFace {
 
     firstCollision(player: Player): Collision {  // oops!  what about when we have gone past the face?
 
+        const epsilon = 0.01;
+        debugger;
         const pos = new Vector2(player.pos.x, player.pos.z);
         const v = new Vector2(Math.sin(player.bearing), -Math.cos(player.bearing))
                     .multiplyScalar(player.movementSpeed);
@@ -38,14 +38,18 @@ export default class VerticalFace {
         }
         const normalTimesRad = normal.clone().multiplyScalar(player.radius);
         const a = this.a.clone().add(normalTimesRad);
-        const playerMinusA = pos.clone().sub(a)
+        const playerMinusA = pos.clone().sub(a);
+        const playerMinusAdotN = playerMinusA.dot(normal);
+        if (playerMinusAdotN < -epsilon) {  // gone past
+            return noCollision;
+        }
         const numer = playerMinusA.dot(u);
         const μ = numer / denom;
         if (μ >= 1 || μ <= 0) {
-            return noCollision;  // misses
+            return noCollision;  // missed
         }
         const collisionPos = bMinusA.clone().multiplyScalar(μ).add(a);
-        const collisionT = - playerMinusA.dot(normal) / vDotNormal;
+        const collisionT = - playerMinusAdotN / vDotNormal;
         const newVelocity = bMinusA.clone().multiplyScalar(
                 v.dot(bMinusA) / bMinusA.lengthSq())
         return { t: collisionT, pos: collisionPos, newVelocity: newVelocity };
