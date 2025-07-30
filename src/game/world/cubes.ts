793030c4ef185b13cc985eac11ec17029872f23b
Dominic Prior <dominicprior42@gmail.com>
2025-07-30 17:25:13 +0100

    Use ascii art in cubes.ts

--- src/game/world/cubes.ts
+++ src/game/world/cubes.ts
@@ -15,48 +15,58 @@ import Player from '../player.js';
 // Also see the matrix maths in https://threejs.org/docs/?q=matr#api/en/math/Matrix4
 // And https://discoverthreejs.com/tips-and-tricks/
 
+interface Cube {
+    centre: Vector2,
+    mesh: Mesh,
+}
+
 export default class Cubes {
-    numRows: number = 2;
+    locations: string = `
+..00
+0..0
+`;
     numBands: number = 5;
-    stride: number = 2;
+    stride: number = 1;
     boxSize: number = 1;
     debug: Debug;
-    meshes: Array<Mesh> = [];
+    cubes: Array<Cube> = [];
+    geom: BoxGeometry = new BoxGeometry(this.boxSize, this.boxSize, this.boxSize,
+                this.numBands, this.numBands, this.numBands);
+    material: MeshStandardMaterial = new MeshStandardMaterial({ color: 'pink', });
 
     constructor(scene: Scene, debug: Debug) {
         this.debug = debug;
-        this.meshes = [];
 
-        const geometry = new BoxGeometry(this.boxSize, this.boxSize, this.boxSize,
-                this.numBands, this.numBands, this.numBands);
-        const material = new MeshStandardMaterial({ color: 'pink', });
-        for (let i=0; i < this.numRows; i++) {
-            this.setMesh(i, geometry, material, scene);
+        const lines = this.locations.trim().split('\n');
+        for (let row = 0; row < lines.length; row++) {
+            const line = lines[row];
+            for (let col = 0; col < line.length; col++) {
+                if (line[col] === '0') {
+                    this.addCube(row, col, scene, lines.length, line.length);
+                }
+            }
         }
     }
 
-    setMesh(i: number, geometry: BoxGeometry, material: MeshStandardMaterial, scene: Scene) {
-        let mesh = new Mesh(geometry, material);
+    addCube(row: number, col: number, scene: Scene, numRows: number, numCols: number) {
+        let mesh = new Mesh(this.geom, this.material);
+        const centre = new Vector2(this.stride * (col - (numCols - 1) / 2),
+                                   this.stride * (row - (numRows - 1) / 2));
         mesh.name = 'cube';
         mesh.frustumCulled = false;
-        mesh.position.x = this.stride * (i - (this.numRows - 1) / 2);
+        mesh.position.x = centre.x;
         mesh.position.y = 0.5;
-        mesh.position.z = 0;
+        mesh.position.z = centre.y;
         mesh.layers.enableAll();
-        // this.mesh.receiveShadow = true
-        this.meshes.push(mesh);
+        // mesh.receiveShadow = true
+        this.cubes.push({centre: centre, mesh: mesh});
         scene.add(mesh);
     }
 
     firstCollision(player: Player): Collision {
         let result = noCollision;
-        for (let i=0; i < this.numRows; i++) {
-            const position = this.meshes[i].position;
-            const box = new HorizontalBox(
-                    new Vector2(position.x, position.z),
-                    this.boxSize,
-                    this.boxSize,
-                    0);
+        for (let cube of this.cubes) {
+            const box = new HorizontalBox(cube.centre, this.boxSize, this.boxSize, 0);
             let collision = box.firstCollision(player);
             if (collision.t < result.t) {
                 result = collision;
