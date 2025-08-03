f16342ed21b70b7f893ac75b204037b89dfff26a
Dominic Prior <dominicprior42@gmail.com>
2025-08-03 13:37:24 +0100

    Add a VerticalFace test to sum.test.ts

--- src/game/utils/sum.test.ts
+++ src/game/utils/sum.test.ts
@@ -1,6 +1,31 @@
+import {
+    Vector2, Vector3,
+} from '../../../three/threebuild/three_module.js';
 import { expect, test } from 'vitest';
 import { sum } from './sum.js';
+import type {
+    Collision,
+} from '../utils/types.js';
+import VerticalFace from '../collisions/verticalface.js';
+import Player from '../player.js';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3)
+test('simple sum', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+
+test('VerticalFace', () => {
+    const face = new VerticalFace(
+            new Vector2(10, 9), new Vector2(14, 9));
+    // @ts-ignore
+    const player: Player = {
+            pos: new Vector3(9, 0, 13),
+            radius: 1,
+            velocity: () => new Vector2(0.2, -0.3),
+        };
+    const collision: Collision = face.firstCollision(player);
+    expect(collision.t).toBe(10);
+    expect(collision.pos.x).toBe(11);
+    expect(collision.pos.y).toBe(10);
+    expect(collision.skidVelocity.x).toBe(0.2);
+    expect(collision.skidVelocity.y).toBe(0);
 });
