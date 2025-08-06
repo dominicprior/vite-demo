3f427b971946b316dd4c8e4f30df16450e744b3d
Dominic Prior <dominicprior42@gmail.com>
2025-08-06 07:57:17 +0100

    Remove the old collisions

--- src/game/player.ts
+++ src/game/player.ts
@@ -6,7 +6,6 @@ import {
 import Keyboard from './utils/keyboard.js';
 import Time from './utils/time.js';
 import World from './world/world.js';
-import type { Collision } from './utils/types.js';
 
 // var _dummy = new Vector3();
 // const upVec = new Vector3(0, 1, 0);
@@ -17,8 +16,6 @@ export default class Player {
     radius: number = 0.25;
     rotationSpeed: number = 1;  // in radians per second
     movementSpeed: number = 2.4;
-    collisionDuration: number = 0.3;
-    bounceFactor: number = 0.7;
     verticalSpeed: number = 1.2;  // for J and K
     gravity: number = 3;
     initialJumpSpeed = 2.5;
@@ -26,9 +23,7 @@ export default class Player {
     // variables
     bearing: number = 0;  // radians from North (negative Z) round towards negative X.
     pos: Vector3 = new Vector3(0, 0.6, 0);
-    collisionTime: number = -100;  // when the last collision occurred.
     jumpTime: number = -100;       // when the last jump occurred.
-    bounceVelocity: Vector2 = new Vector2();
     verticalVelocity = 0;
 
     keyboard: Keyboard;
@@ -56,51 +51,19 @@ export default class Player {
     }
 
     update() {
-        const delta = this.time.delta;
-        const bouncing = this.time.elapsed < this.collisionTime + this.collisionDuration;
-
         this.updateTurning();
         this.updateUpDown();
-        if (bouncing) {
-            // bouncing, ignore movement keys
-            this.pos.x += this.bounceVelocity.x * delta;
-            this.pos.z += this.bounceVelocity.y * delta;
-        }
-        else {
-            this.updateMoving();
-            this.updateStrafing();
-        }
+        this.updateMoving();
+        this.updateStrafing();
         this.updateJumping();
     }
 
     updateMoving() {
         const delta = this.time.delta;
         const moving = this.keyboard.moving();
-        if (moving) {
-            const collision = this.world.firstCollision(this);
-            if (collision.t < delta) {
-                this.collide(collision);
-            }
-            else {
-                const velocity = this.velocity();
-                this.pos.x += delta * moving * velocity.x;
-                this.pos.z += delta * moving * velocity.y;
-            }
-        }
-    }
-
-    collide(collision: Collision) {
-        const skid = collision.skidVelocity;
-        this.bounceVelocity = skid.clone()  // (skid - v) * bounceFactor  +  skid
-                .sub(this.velocity())
-                .multiplyScalar(this.bounceFactor)
-                .add(skid);
-        const newPos = this.bounceVelocity.clone()  // bounceVelocity * "the remaining time" + collision.pos
-                .multiplyScalar(this.time.delta - collision.t)
-                .add(collision.pos);
-        this.pos.x = newPos.x;
-        this.pos.z = newPos.y;
-        this.collisionTime = this.time.elapsed;  // ? plus spare time?
+        const velocity = this.velocity();
+        this.pos.x += delta * moving * velocity.x;
+        this.pos.z += delta * moving * velocity.y;
     }
 
     updateTurning() {
