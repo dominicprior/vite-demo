383a40fe288f096b7e618a11bde7dd1e94d48ef8
Dominic Prior <dominicprior42@gmail.com>
2025-08-07 10:41:55 +0100

    Add more crosshairs

--- src/game/world/crosshairs.ts
+++ src/game/world/crosshairs.ts
@@ -1,6 +1,7 @@
 import {
     Scene, Mesh, MeshBasicMaterial,
     BoxGeometry,
+    Vector3,
 } from '../../../three/threebuild/three_module.js';
 import Debug from '../utils/debug.js';
 import Player from '../player.js';
@@ -8,31 +9,50 @@ import Player from '../player.js';
 export default class CrossHairs {
     boxSize: number = 0.008;
     debug: Debug;
-    meshes: Array<Mesh> = [];
+    meshes: Array<Array<Array<Mesh>>> = [];
 
     constructor(scene: Scene, debug: Debug) {
         this.debug = debug;
 
         const material = new MeshBasicMaterial({ color: 'red', });
-        for (let i=0; i<2; i++) {
-            const geometry = new BoxGeometry(
-                i ? this.boxSize / 10 : this.boxSize,
-                i ? this.boxSize      : this.boxSize / 10,
-                this.boxSize / 10
-            );
-            const mesh = new Mesh(geometry, material);
-            mesh.name = 'crosshair' + i;
-            mesh.layers.enableAll();
-            scene.add(mesh);
-            this.meshes.push(mesh);
+        for (let axis of [0, 1, 2]) {
+            const aa: Array<Array<Mesh>> = [];
+            for (let sign of [-1, 1]) {
+                const a: Array<Mesh> = [];
+                for (let i of [0, 1]) {
+                    const geometry = new BoxGeometry(
+                        i ? this.boxSize / 10 : this.boxSize,
+                        i ? this.boxSize      : this.boxSize / 10,
+                        this.boxSize / 10
+                    );
+                    const mesh = new Mesh(geometry, material);
+                    mesh.name = 'crosshair' + axis + sign + i;
+                    mesh.frustumCulled = false;
+                    mesh.layers.enableAll();
+                    scene.add(mesh);
+                    a.push(mesh);
+                }
+                aa.push(a);
+            }
+            this.meshes.push(aa);
         }
     }
 
     update(player: Player) {
-        const vec = player.forwardsDirection().clone().multiplyScalar(0.2);
-        for (let mesh of this.meshes) {
-            mesh.position.copy(player.pos.clone().add(vec));
-            mesh.rotation.y = player.bearing;
+        for (let [axis, relPos] of [
+                                    [0, player.forwardsDirection()],
+                                    [1, player.strafeDirection()],
+                                    [2, new Vector3(0,1,0)]
+                                ]) {
+            const aa: Array<Array<Mesh>> = this.meshes[axis as number];
+            for (let sign of [0, 1]) {
+                const a: Array<Mesh> = aa[sign];
+                const signedRelPos = (relPos as Vector3).clone().multiplyScalar((sign * 2 - 1) * 0.2);
+                for (let mesh of a) {
+                    mesh.position.copy(player.pos.clone().add(signedRelPos));
+                    mesh.rotation.y = player.bearing;
+                }
+            }
         }
     }
 }
