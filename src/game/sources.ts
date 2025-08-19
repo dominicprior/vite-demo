7a2d48ece700e40fec2c2d6ead705889333c0fac
Dominic Prior <dominicprior42@gmail.com>
2025-08-19 18:20:41 +0100

    Remove all the entries from sources

--- src/game/sources.ts
+++ src/game/sources.ts
@@ -1,13 +1,14 @@
-export default [ {
-    name: 'environmentMapTexture',
-    type: 'cubeTexture',
-    path: [
-        'textures/environMap/px.jpg',
-        'textures/environMap/nx.jpg',
-        'textures/environMap/py.jpg',
-        'textures/environMap/ny.jpg',
-        'textures/environMap/pz.jpg',
-        'textures/environMap/nz.jpg',
-    ]
-}
+export default [
+// {
+//     name: 'environmentMapTexture',
+//     type: 'cubeTexture',
+//     path: [
+//         'textures/environMap/px.jpg',
+//         'textures/environMap/nx.jpg',
+//         'textures/environMap/py.jpg',
+//         'textures/environMap/ny.jpg',
+//         'textures/environMap/pz.jpg',
+//         'textures/environMap/nz.jpg',
+//     ]
+// }
 ]
